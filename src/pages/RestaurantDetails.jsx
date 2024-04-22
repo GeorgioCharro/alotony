9498020162aa5a067ae6ca9cfe66ad631d963395
georgiocharro@hotmail.com <georgiocharro@hotmail.com>
2024-04-22 21:40:06 +0300

    Module changes

--- src/pages/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import restaurants from '../restaurants.json';
-import ImageGallery from 'react-image-gallery';
-import 'react-image-gallery/styles/css/image-gallery.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import Whatsapp from '@mui/icons-material/WhatsApp';
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 import IconButton from '@mui/material/IconButton';
+
+// Install Swiper modules
+
 
 const RestaurantDetails = () => {
   const { name } = useParams();
@@ -18,7 +25,6 @@ const RestaurantDetails = () => {
     return <p>Restaurant not found!</p>;
   }
 
-  // Dynamically create the images array based on available images
   const images = [];
   let i = 1;
   while (true) {
@@ -26,30 +32,22 @@ const RestaurantDetails = () => {
       const image = require(`../assets/jpg/${restaurant.image}Menu${i}.jpeg`);
       images.push({ original: image, thumbnail: image });
       i++;
-      if (i > 4) break; // Adjust this if the number of images varies
+      if (i > 8) break;
     } catch (e) {
-      break; // Stop the loop if there are no more images
+      break;
     }
   }
 
   const handleWhatsAppClick = () => {
-    window.location.href = 'https://api.whatsapp.com/send/?phone=96170427505&text&type=phone_number&app_absent=0'; // Adjust number as needed
+    window.location.href = 'https://api.whatsapp.com/send/?phone=96170427505&text&type=phone_number&app_absent=0';
   };
 
   const handleCallClick = () => {
-    window.location.href = 'tel:+96170427505'; // Adjust number as needed
-  };
-
-  const renderFullscreenButton = (onClick, isFullscreen) => {
-    return null; // Hide the fullscreen button
-  };
-
-  const renderPlayPauseButton = (onClick, isPlaying) => {
-    return null; // Hide the slideshow button
+    window.location.href = 'tel:+96170427505';
   };
 
   return (
-    <div className='mb-8'>
+    <div className='mb-20'>
       <div className='flex justify-between items-center p-4'>
         <div className='flex items-center'>
           <KeyboardBackspaceIcon className='mr-4 text-yellow' sx={{ width: '60px', height: '60px' }} onClick={() => navigate(-1)} />
@@ -57,14 +55,23 @@ const RestaurantDetails = () => {
         </div>
       </div>
       
-      <ImageGallery
-        items={images} 
-        renderFullscreenButton={renderFullscreenButton} 
-        renderPlayPauseButton={renderPlayPauseButton} 
-      />
+      <Swiper
+        modules={[Navigation, Pagination,Scrollbar, A11y]}
+        spaceBetween={10} // Reduced space between slides
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        className="max-h-[70vh] mx-auto" // Adjusted max height and centered
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img src={img.original} alt={`Slide ${index}`} className="max-h-[70vh] mx-auto" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-      {/* Icons at the bottom right */}
-      <div style={{ position: 'fixed', bottom: 20, right: 20, display: 'flex', gap: '10px' }}>
+      <div className="fixed bottom-5 right-5 flex gap-2.5 z-50">
         <IconButton onClick={handleWhatsAppClick} sx={{ color: '#fff', backgroundColor: '#F29F05', '&:hover': { backgroundColor: '#db8500' } }}>
           <Whatsapp />
         </IconButton>
